94fa1ce87e5708a9af5f32ca24bc8c0abb6f126a
Manjunath-SS <mssangashetty@gmail.com>
2017-05-20 23:41:13 +0530

    Color Palette 8/8 Completely functional along with 2 color comparison

--- exp/quadcirvalues.cpp
+++ exp/quadcirvalues.cpp
@@ -12,15 +12,30 @@ int main()
     x=960.0;
 	y=540.0;
 	//printf("%d",sizeof(a)/sizeof(a[0]));
-    for(i=0;i<1920;i++)
-        for(j=0;j<1080;j++)
+    for(i=960;i<1500;i++)
+        for(j=0;j<540;j++)
     {
     	n=i-960;
     	p=fabs(n);
-        if(i<x && j>=y && p >=j-540)
+        if(i>=x && j<y && p >= 540-j)
         {
-            ra=(x-i)*(x-i)+(j-y)*(j-y);
+            ra=(i-x)*(i-x)+(y-j)*(y-j);
             ra=sqrt(ra);
+//            if(ra>100 && ra<=125)
+//            if(ra>125 && ra<=150)
+//            if(ra>150 && ra<=175)
+//            if(ra>175 && ra<=200)
+//            if(ra>200 && ra<=225)
+//            if(ra>225 && ra<=250)
+//            if(ra>250 && ra<=275)
+//            if(ra>275 && ra<=300)
+//            if(ra>300 && ra<=325)
+//            if(ra>325 && ra<=350)
+//            if(ra>350 && ra<=375)
+//            if(ra>375 && ra<=400)
+//            if(ra>400 && ra<=425)
+//            if(ra>425 && ra<=450)
+//            if(ra>450 && ra<=475)
             if(ra>475 && ra<=500)
                 {
 					mydraw(i,j);
