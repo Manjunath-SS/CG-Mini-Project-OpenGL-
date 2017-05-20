b1d5d4e4abc3caf381cd5ff8462e9a221c9c555e
Manjunath-SS <mssangashetty@gmail.com>
2017-05-20 22:18:29 +0530

    Color Palette 4/8 Completely Functional

--- exp/quadcirvalues.cpp
+++ exp/quadcirvalues.cpp
@@ -8,22 +8,25 @@ void mydraw(int i, int j)
 
 int main()
 {
-	float x,y,i,j,ra;
+	float x,y,i,j,ra,n,p;
     x=960.0;
 	y=540.0;
 	//printf("%d",sizeof(a)/sizeof(a[0]));
-    for(i=960;i<1920;i++)
-        for(j=540;j<1080;j++)
+    for(i=0;i<1920;i++)
+        for(j=0;j<1080;j++)
     {
-        if(i>=x && j>=y && i-960<j-540)
+    	n=i-960;
+    	p=fabs(n);
+        if(i<x && j>=y && p >=j-540)
         {
-            ra=(i-x)*(i-x)+(j-y)*(j-y);
+            ra=(x-i)*(x-i)+(j-y)*(j-y);
             ra=sqrt(ra);
-            if(ra>=475 && ra<=500)
+            if(ra>475 && ra<=500)
                 {
 					mydraw(i,j);
                 }
         }
     }
+
 }
 
